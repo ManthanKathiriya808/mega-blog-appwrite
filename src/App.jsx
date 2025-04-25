c20ac40d9b196884f6f2d2b871ab771e845121a4
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2025-04-25 14:21:20 +0530

    asdgadfg

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import {Footer, Header} from './components/index'
+import { Outlet } from 'react-router-dom'
+// import {login,logout} from '.store/authSlice'
 
 function App() {
 
-console.log()
-  return (
+  const [loading,setLoading] = useState(true)
 
-    <>
-     
-        <h1>blog with app write</h1>
+  const dispatch = useDispatch()
 
-     </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+        if(userData){
+            dispatch(login({userData}))
+        }
+        else{
+          dispatch(logout())
+        }
+    })
+    .finally(()=> setLoading(false))
+  },[])
+
+
+
+    return !loading ? (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header/>
+          <main>
+            TOdo : <Outlet/>
+          </main>
+          <Footer/>
+        </div>
+      </div>
+    ) : null
 }
 
 export default App
